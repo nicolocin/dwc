c011e333ee04d22fd15f5130ff984ba8bb01f036
Nicole Lo <nlo@mit.edu>
2019-11-13 16:38:51 -0500

    set up cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='dwc',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Distributed Weighted Consolidation',
+    author='Nicole Lo',
+    license='MIT',
+)
